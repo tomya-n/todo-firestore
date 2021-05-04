98d5e555dfb094727982b571ab89c427b92a4f95
tom <tomcan1126@gmail.com>
2021-05-05 07:16:56 +0900

    データの削除が可能になった。

--- src/App.vue
+++ src/App.vue
@@ -23,10 +23,9 @@ export default {
   },
   data(){
     return {
-      todos: firebase.database().ref("test").once("child_added",function(obj){
-        console.log(obj.key);
-        console.log(obj.val());
-      })
+      todos: firebase.database().ref("test").once("value",function(obj){
+        return obj.val();
+      }),
     }
   },
   methods:{
@@ -35,16 +34,24 @@ export default {
       if(value){
         db.ref("test/").push(
           {text: value, complete: false}
-        )
+        ).then(response => {
+          console.log(response , "登録成功！");
+        })
         // this.todos.unshift({text: value, complete: false});
       }
     },
     handleParentDeleteTodo(index){
-      this.todos.splice(index,1);
+      // this.todos.splice(index,1);
+      const db = firebase.database();
+      db.ref("test/").child(index).remove();
     },
     handleParentCompleteTodo(index){
       this.todos[index].complete = !this.todos[index].complete;
     }
+  },
+  mounted(){
+    firebase.database().ref("test")
+      .on("value", snapshot => (this.todos = snapshot.val()));
   }
 }
 </script>
